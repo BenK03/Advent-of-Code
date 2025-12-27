321d3175f06be47e97f581fe8bc4c49bc3ab3029
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-26 21:16:47 -0500

    Completed day02 part2 in Python

---
+++ 2025/day02/Python/part2/day02pt2.py
@@ -0,0 +1,26 @@
+def main():
+    ans = 0
+
+    with open("input.txt", "r", encoding="utf-8") as file:
+        s = file.read()
+
+    for part in s.split(","):
+        start, end = map(int, part.split("-"))
+
+        for i in range(start, end + 1):
+            s = str(i)
+            n = len(s)
+
+            for i in range(1, len(s) // 2 + 1):
+
+                pattern = s[:i]
+                repeats =  n // i
+
+                # check if pattern repeated is the same as s
+                if pattern * repeats == s: # if yes invalid
+                    ans += int(s)
+                    break
+
+    print(ans)
+
+main()
